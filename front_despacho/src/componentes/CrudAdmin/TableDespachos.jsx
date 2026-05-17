72839b3dbfdd4ff4241adc6ba18b9e60607e3747
Nicole <nc.2012.nicole@gmail.com>
2026-05-17 16:50:05 -0400

    cambiar a ip publica del front

--- front_despacho/src/componentes/CrudAdmin/TableDespachos.jsx
+++ front_despacho/src/componentes/CrudAdmin/TableDespachos.jsx
@@ -8,7 +8,7 @@ export const TableDespachos = () => {
 
   const despacho = async () => {
     await axios
-      .get("http://192.168.3.20/api/v1/despachos", {
+      .get("http://34.200.243.229/api/v1/despachos", {
         headers:{
               'Content-Type': 'application/json',
               'Accept': 'application/json'
