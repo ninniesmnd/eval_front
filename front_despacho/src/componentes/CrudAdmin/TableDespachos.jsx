7575037a02066b185e644f9cb2f646d0bc5b14c3
Nicole <nc.2012.nicole@gmail.com>
2026-05-18 17:45:27 -0400

    fix: cambios en api

--- front_despacho/src/componentes/CrudAdmin/TableDespachos.jsx
+++ front_despacho/src/componentes/CrudAdmin/TableDespachos.jsx
@@ -8,7 +8,7 @@ export const TableDespachos = () => {
 
   const despacho = async () => {
     await axios
-      .get("http://34.201.209.81/api/v1/despachos", {
+      .get("/api/v1/despachos", {
         headers:{
               'Content-Type': 'application/json',
               'Accept': 'application/json'
