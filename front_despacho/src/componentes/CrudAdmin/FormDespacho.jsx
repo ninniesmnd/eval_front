8a5b53156ac94a8d55c552f65cfdc6e151e68d45
Nicole <nc.2012.nicole@gmail.com>
2026-07-12 19:27:18 -0400

    prueba

--- front_despacho/src/componentes/CrudAdmin/FormDespacho.jsx
+++ front_despacho/src/componentes/CrudAdmin/FormDespacho.jsx
@@ -99,7 +99,7 @@ export const FormDespacho = ({ venta, onClose }) => {
           />
         </div>
         <div className="mb-5">
-          <label className="block font-bold mb-2">Valor de compra</label>
+          <label className="block font-bold mb-2">Valor de compras</label>
           <input
             type="number"
             value={venta.valorCompra}
