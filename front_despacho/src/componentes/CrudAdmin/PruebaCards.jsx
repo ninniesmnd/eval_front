da26137539b86743b039419681059472b6804852
Nicole <nc.2012.nicole@gmail.com>
2026-07-12 21:11:02 -0400

    Validar pipelines

--- front_despacho/src/componentes/CrudAdmin/PruebaCards.jsx
+++ front_despacho/src/componentes/CrudAdmin/PruebaCards.jsx
@@ -11,7 +11,7 @@ export const PruebaCards = () => {
     <section>
       <div className="flex justify-center">
         <CardComponent
-          title="hola, soy antonio"
+          title="Ordenes de compra"
           description="Revisa las últimas oc realizadas para generar su despacho"
           buttonText="Consultar"
           onClick={() => {
@@ -20,7 +20,7 @@ export const PruebaCards = () => {
           }}
         />
         <CardComponent
-          title="hola, soy antonio"
+          title="Ordenes de despacho"
           description="Consulta los despachos realizados, modifica los registros de intentos o cierra la orden"
           buttonText="Consultar"
           onClick={() => {
